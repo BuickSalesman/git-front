df40913d716b1f697de0cb3a3f56b175a5c08dbe
BuickSalesman <adam.odonnell.e@gmail.com>
2025-08-09 23:17:05 -0400

    added content

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
 import { Header } from './components/Header/Header.jsx'
 import { Footer } from './components/Footer/Footer.jsx'
 import './App.css'
 
 function App() {
-    const [count, setCount] = useState(0)
-
     return (
-        <>
+        <div>
             <Header />
             <Footer />
-        </>
+        </div>
     )
 }
 

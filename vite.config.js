d9b344e56c9fe871631ea95ed3c209fba83859aa
BuickSalesman <adam.odonnell.e@gmail.com>
2025-08-11 13:38:10 -0400

    finally got it up and running

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    optimizeDeps: {
-        disabled: true,
-    },
     plugins: [react()],
 })

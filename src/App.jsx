0e0797fe6910bd419a07dac378da5c88d1cfff6e
BuickSalesman <adam.odonnell.e@gmail.com>
2025-08-13 11:31:27 -0400

    starting styling work

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Header } from './components/Header/Header.jsx'
 import { Footer } from './components/Footer/Footer.jsx'
+import { Content } from './components/Content/Content.jsx'
+
 import './App.css'
 
 function App() {
     return (
         <div>
             <Header />
+            <Content />
             <Footer />
         </div>
     )
---
+++ src/components/Content/Content.jsx
@@ -0,0 +1,7 @@
+import "./Content.css"
+
+export function Content() {
+    return (
+        <div className="content">yo whaddup son</div>
+    )
+}

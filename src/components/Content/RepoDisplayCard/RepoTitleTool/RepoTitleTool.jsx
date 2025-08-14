48b87b9f65bfd65726d04f7ca29d1023ab6db3d5
BuickSalesman <adam.odonnell.e@gmail.com>
2025-08-14 19:15:42 -0400

    opening and closing heatmaps

--- src/components/Content/RepoDisplayCard/RepoTitleTool/RepoTitleTool.jsx
+++ src/components/Content/RepoDisplayCard/RepoTitleTool/RepoTitleTool.jsx
@@ -6,6 +6,17 @@ import { Heatmap } from "../Heatmap/Heatmap.jsx"
 import { QuickCommitButton } from "./QuickCommitButton/QuickCommitButton.jsx"
 import { MultiCommitTool } from "./MultiCommitTool/MultiCommitTool.jsx"
 
+function handleHeatmapDropdown(e) {
+    const parentCard = e.currentTarget.closest(".repo-card")
+
+    if (parentCard.classList.contains("open-card")) {
+        parentCard.classList.remove("open-card")
+        return
+    }
+
+    parentCard.classList.add("open-card")
+}
+
 export function RepoTitleTool({ id }) {
     return (
         <div className="title-tool">
@@ -15,7 +26,10 @@ export function RepoTitleTool({ id }) {
             </div>
             <div className="title-tool-left">
                 <div className="swipe-indicator">...</div>
-                <button className="heatmap-dropdown-button">
+                <button
+                    className="heatmap-dropdown-button"
+                    onClick={(e) => handleHeatmapDropdown(e)}
+                >
                     <Heatmap />
                 </button>
             </div>

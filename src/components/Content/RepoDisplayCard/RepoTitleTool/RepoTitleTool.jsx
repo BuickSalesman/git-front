2a64344a8c05e4373518bfd718e7aa51a60b9e4a
BuickSalesman <adam.odonnell.e@gmail.com>
2025-08-15 22:31:01 -0400

    set up svg overlay

--- src/components/Content/RepoDisplayCard/RepoTitleTool/RepoTitleTool.jsx
+++ src/components/Content/RepoDisplayCard/RepoTitleTool/RepoTitleTool.jsx
@@ -2,11 +2,13 @@ import "./RepoTitleTool.css"
 
 import { TitleToolLeft } from "./TitleToolLeft/TitleToolLeft.jsx"
 import { TitleToolRight } from "./TitleToolRight/TitleToolRight.jsx"
+import { TitleToolSVG } from "./TitleToolSVG/TitleToolSVG.jsx"
 
 
 export function RepoTitleTool({ id }) {
     return (
         <div className="title-tool">
+            <TitleToolSVG />
             <TitleToolLeft />
             <div className="title-tool-center">
                 <div className="repo-name">Repo Name: {id}</div>
---
+++ src/components/Content/RepoDisplayCard/RepoTitleTool/TitleToolSVG/TitleToolSVG.jsx
@@ -0,0 +1,9 @@
+import "./TitleToolSVG.css"
+
+export function TitleToolSVG() {
+    return (
+        <div className="title-tool-svg-layer">
+            <svg id="title-tool-svg"></svg>
+        </div>
+    )
+}

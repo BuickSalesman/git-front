f7dd9fe25c9f2ffff6393e9cd92b8d174aa7c832
BuickSalesman <adam.odonnell.e@gmail.com>
2025-08-16 01:44:56 -0400

    styled multi commit tool placeholder - about to start on sliding drawer for dropdown

--- src/components/Content/RepoDisplayCard/RepoDropdownDisplay/RepoDropdownDisplay.jsx
+++ src/components/Content/RepoDisplayCard/RepoDropdownDisplay/RepoDropdownDisplay.jsx
@@ -4,9 +4,11 @@ import { Heatmap } from "../Heatmap/Heatmap.jsx"
 
 export function RepoDropdownDisplay() {
     return (
-        <div className="repo-dropdown-display">
-            <div className="heatmap-container">
-                <Heatmap />
+        <div className="repo-dropdown-wrapper">
+            <div className="repo-dropdown-display">
+                <div className="heatmap-container">
+                    <Heatmap />
+                </div>
             </div>
         </div>
     )

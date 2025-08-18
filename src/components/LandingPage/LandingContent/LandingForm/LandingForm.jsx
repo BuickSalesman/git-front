990946c9dd9e8bcc90051b613094ec86175398c6
BuickSalesman <adam.odonnell.e@gmail.com>
2025-08-18 19:24:47 -0400

    basic scaffold of form done

--- src/components/LandingPage/LandingContent/LandingForm/LandingForm.jsx
+++ src/components/LandingPage/LandingContent/LandingForm/LandingForm.jsx
@@ -1,7 +1,22 @@
 import "./LandingForm.css"
 
 export function LandingForm() {
+
+    const handleSubmit = () => { return }
+
     return (
-        <div>yo whaddup</div>
+        <div className="landing-form-container">
+            <div>errors go here</div>
+            <form className="landing-form" onSubmit={handleSubmit}>
+                <div>username</div>
+                <input type="text" />
+                <div>password</div>
+                <input type="text" />
+                <div>confirmation</div>
+                <input type="text" />
+            </form>
+            <button type="submit">sign up</button>
+            <button type="submit">log in</button>
+        </div>
     )
 }
